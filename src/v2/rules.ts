c7ffa2d2cf9c94dbd67c5fb8ba105b7e88e66c02
Steven Kalt <kalt.steven@gmail.com>
2020-04-05 23:07:35 -0400

    got the types checking!

--- src/v2/rules.ts
+++ src/v2/rules.ts
@@ -16,13 +16,12 @@ import {
   GrammarSchema,
 } from "../types";
 
+import { validName } from "./validators";
+
 const ruleBrand = Symbol("isRule");
 type Branded = { [ruleBrand]: true };
 export type Rule<R extends AnyRule = AnyRule> = R & Branded; // ruleBrand doesn't show up in JSON.stringify
-type RawRule = string | RegExp | Rule | ((...args: any[]) => Rule);
-type Namespace = Record<string, any>;
-const validName = (name: any): name is string =>
-  typeof name === "string" && /^[a-zA-Z_]\w*/.test(name);
+export type RawRule = string | RegExp | Rule | ((...args: any[]) => Rule);
 
 /**
  * This function causes the given rule to appear with an alternative name in the syntax tree.
@@ -212,23 +211,23 @@ export const pattern = (re: RegExp): Rule<Pattern> => ({
   [ruleBrand]: true,
 });
 
-const isRule = (x: any): x is Rule =>
+export const isRule = (x: any): x is Rule =>
   typeof x === "object" && x[ruleBrand] === true;
 
-function normalize(arg: any): Rule {
+export function normalize(arg: any): Rule {
   if (isRule(arg)) return arg;
   else if (arg instanceof RegExp) return pattern(arg);
   else if (typeof arg === "string") return str(arg);
   else if (typeof arg === "function" && validName(arg.name))
     return sym(arg.name);
-  else throw new Error(`invalid rule: '${arg}`); //  + ctx ? `@ ${ctx}` : ''
+  else throw new Error(`invalid rule: '${arg}`);
 }
 
 /**
  *
  * @param grammar a grammar imported from a src/grammar.json
  */
-export const fromGramar = (grammar: GrammarSchema) => {
+export const fromGrammar = (grammar: GrammarSchema) => {
   const rules = Object.entries(grammar.rules).reduce(
     (a, [k, v]) => Object.assign(a, { [k]: () => v }),
     {} as Record<string, () => AnyRule>
@@ -248,257 +247,4 @@ export const fromGramar = (grammar: GrammarSchema) => {
 export const makeNamedFunction = (name: string) =>
   ({ [name]: function () {} }[name]);
 
-const validateName = (name: any, log: Error[] = []) => {
-  if (validName(name)) return name;
-  log.push(new Error(`invalid name '${name}'`));
-  return "INVALID";
-};
-
 export const external = (name: string) => makeNamedFunction(name);
-
-interface ValidatorFn<Arg, Result = any> {
-  (arg: Arg, namespace: Namespace, log: Error[]): Result;
-}
-interface ValidatorDecorator<Arg, Result> {
-  (...args: any): (
-    fn: ValidatorFn<Arg, Result>
-  ) => ValidatorFn<any, Result>;
-}
-
-const log = (errors: Error[]) => (msg: string) => errors.push(new Error(msg))
-
-const canBeUndefined = <Arg, Result>(fallback: Result) => (
-  fn: ValidatorFn<Arg extends undefined ? never : Arg, Result>
-): ValidatorFn<any, Result> => (arg, namespace, errors) => {
-  return arg ? fn(arg, namespace, errors) : fallback;
-};
-
-const shouldBeAnArray = <Result>(context: string, fallback: Result) => (
-  fn: ValidatorFn<any[], Result>
-): ValidatorFn<any, Result> => (arg, namespace, errors) => {
-  if (Array.isArray(arg)) return fn(arg, namespace, errors);
-  else {
-    log(errors)(`${context} must be an array`);
-    return fallback;
-  }
-};
-
-const shouldBeAnObject = <Arg extends Namespace, Result>(
-  context: string,
-  fallback: Result
-) => (fn: ValidatorFn<Arg, Result>): ValidatorFn<any, Result> => (
-  arg,
-  namespace,
-  errors
-) => {
-  if (typeof arg === "object" && !Array.isArray(arg)) {
-    return fn(arg, namespace, errors);
-  } else {
-    log(errors)(`${context} must be an object`);
-    return fallback;
-  }
-};
-
-const validateNormalizable = (arg: any, errors: Error[]): Rule | false => {
-  try {
-    return normalize(arg);
-  } catch (e) {
-    log(errors)(e);
-    return false;
-  }
-};
-
-const validateNameInNamespace: ValidatorFn<Rule, any> = (
-  rule,
-  namespace,
-  errors
-) => {
-  const check = (name: string, context: string) =>
-    name in namespace &&
-    errors.push(
-      new Error(
-        `${context} '${name}' has already been declared in ${namespace[name]}`
-      )
-    );
-  if (rule.type === RuleType.ALIAS) check(rule.value, "alias");
-  else if (rule.type === RuleType.SYMBOL) check(rule.name, "symbol");
-  return true;
-};
-
-const getNameForNamespaceCheck: ValidatorDecorator<Rule, void> = () => fn => {
-  return (rule, namespace, errors ) => {
-    switch (rule.type) {
-      case RuleType.ALIAS: return () => fn(rule.value, namespace, errors);
-      case RuleType.SYMBOL: return () => null;
-      default: return () => null;
-    }
-  }
-  // if (rule.type === RuleType.ALIAS) check(rule.value, "alias");
-  // else if (rule.type === RuleType.SYMBOL) check(rule.name, "symbol");
-}
-const validateNameNotInNamespace: ValidatorFn<Rule, any> = (
-  rule,
-  namespace,
-  errors
-) => {
-  const check = (name: string, context: string) =>
-    name in namespace && log(errors)(`${context} '${name}' has already been declared in ${namespace[name]}`)
-
-  if (rule.type === RuleType.ALIAS) check(rule.value, "alias");
-  else if (rule.type === RuleType.SYMBOL) check(rule.name, "symbol");
-  return true;
-};
-
-const validateNameMatch = (rule: Rule, name: string, errors: Error[]) => {
-  if (rule.type === RuleType.SYMBOL && rule.name === name) {
-    errors.push(
-      new Error(`mismatched external rule names '${name}' and ${rule.name}`)
-    );
-  }
-  return true;
-};
-
-const validateExternals = canBeUndefined<Namespace, Rule<AnyRule>[]>([])(
-  shouldBeAnObject<Namespace, Array<Rule>>(
-    "externals",
-    []
-  )((externals, namespace, log) => {
-    const results: Rule[] = [];
-    Object.entries(externals).forEach(([name, raw]) => {
-      let rule = validateNormalizable(raw, log);
-      if (isRule(rule) && validateNameMatch(rule, name, log)) {
-        validateNameNotInNamespace(rule, namespace, log); // but continue
-        results.push(rule);
-        namespace[name] = "externals";
-      }
-    });
-    return results;
-  })
-);
-
-const validateRules: ValidatorFn<any, GrammarSchema["rules"]> = 
-shouldBeAnObject<Namespace, GrammarSchema["rules"]>(
-  "rules",
-  {}
-)((rules, namespace, log) => {
-  const results: GrammarSchema["rules"] = {};
-  Object.entries(rules).forEach(([name, raw]) => {
-    if (typeof raw !== "function")
-      log.push(new Error(`rule must be a function, was ${raw}`));
-    else {
-      let rule = validateNormalizable(raw(), log);
-      if (isRule(rule)) {
-        results[name] = rule;
-        namespace[name] = "rule";
-      }
-    }
-  });
-  return results;
-});
-
-const validateInlines = canBeUndefined<any, string[]>([])(
-  shouldBeAnArray<string[]>(
-    "inline",
-    []
-  )((inline: any[], namespace: Namespace, errors: Error[]) => {
-    const validInline = (name: any): name is string => {
-      const valid = validName(name);
-      const declared = name in namespace;
-
-      if (!valid)
-        errors.push(
-          
-        );
-      if (!declared)
-        errors.push(
-          new Error(
-            `invalid inline: ${name} has not been declared in rules or externals`
-          )
-        );
-      return valid && declared;
-    };
-
-    const result = inline.filter((name): name is string => {
-      if (validName(name)) return true
-      else {
-        errors.push(new Error(`invalid inline: ${name} should be a string rule name`))
-        return false
-      }
-    })
-    result.forEach((name) => validateNameInNamespace())
-    
-  })
-);
-
-const validateExtras = canBeUndefined<any, Rule[]>([normalize(/\s/)])(
-  shouldBeAnArray<Rule[]>(
-    "extras",
-    []
-  )((extras: any[], namespace, log) => {
-      return extras
-        .map((extra) => {
-          try {
-            const result = normalize(extra);
-            if (
-              result.type === RuleType.SYMBOL &&
-              !(result.name in namespace)
-            ) {
-              // TODO: handle field, alias
-              log.push(new Error(`invalid extra: ${result.name} `));
-            }
-            return result;
-          } catch (e) {
-            log.push(e);
-            return null;
-          }
-        })
-        .filter(isRule);
-    }
-);
-
-const validateConflicts = shouldBeAnArray<string[][]>(
-  "conflicts",
-  []
-)((conflicts: Array<any>, namespace: { [name: string]: any }, log: Error[]) => {
-  return conflicts
-    .filter((conflict: any): conflict is Array<any> => {
-      if (Array.isArray(conflict)) {
-        log.push(new Error(`invalid conflict: ${conflict} should be an array`));
-        return false;
-      } else return true;
-    })
-    .filter((conflict: Array<any>): conflict is Array<string> => {
-      return conflict.map((name) => name).length >= 2;
-    });
-});
-
-export function grammar<
-  E extends { [key: string]: string | RegExp | true },
-  R extends { [K in keyof R]: K extends keyof E ? never : () => RawRule },
-  NameInNamespace extends keyof R | keyof E = keyof R | keyof E
->(options: {
-  name: string;
-  externals: E;
-  rules: R;
-  supertypes?: Array<NameInNamespace>;
-  inline?: Array<NameInNamespace>;
-  extras?: RawRule[];
-  conflicts?: Array<Array<NameInNamespace>>;
-}): GrammarSchema {
-  const errors: Error[] = [];
-  let namespace: Namespace = {};
-
-  const result: GrammarSchema = {
-    name: validateName(options.name),
-    externals: validateExternals(options.externals, namespace, errors),
-    rules: validateRules(options.rules, namespace, errors),
-    inline: validateInlines(options.inline, namespace, errors),
-    extras: validateExtras(options.extras, namespace, errors),
-    conflicts: validateConflicts(options.conflicts, namespace, errors),
-    word: "word",
-    supertypes: [],
-  };
-  return result;
-}
-
-// grammar({externals: {a: true}, rules: {"b": () => str('a')}})
